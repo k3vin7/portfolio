b80d7cf328dade13b09c674536e255b27e93d788
kevinc02@naver.com <kevinc02@naver.com>
2025-05-20 15:18:10 +0900

    skills line 1-5

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -28,7 +28,7 @@ export default function Skills() {
         px-4"
         id="skills"
         >
-            <div className='
+        {/* <div className='
             absolute
             '>
                 <img src={ lines } 
@@ -36,67 +36,139 @@ export default function Skills() {
                 className='
                 h-[70vh]
                 '/>
-            </div>
-            <div className='
-            flex
-            '>
-                {/* 프론트엔드 */}
-                <div className='
-                absolute
-                top-[11vh] left-[21vw]
-                rounded-full
-                h-[25vh] w-[25vh]
-                flex flex-col items-center justify-center
-                bg-black border-white border-4
-                transition-all ease-in-out duration-500
-                transform hover:scale-[2.8]
-                origin-top-right
-                group
-                '>
-                    {/* 기본 텍스트 */}
-                    <div>
-                        <p className='
-                        font-semibold text-2xl
-                        group-hover:opacity-0
-                        transition-all duration-300
-                        '>Front-End</p>
-                    </div>
-                    {/* 프론트엔드 리스트 */}
-                    <div>
-                        
-                    </div>
-                </div>
+            </div>*/}
+        <div className="flex items-center justify-center h-screen bg-black">
+            <svg
+            viewBox="0 0 800 600"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-full h-full bg-black"
+            >
+                <text
+                    x="400"
+                    y="300"
+                    fill="white"
+                    font-size="40"
+                    font-family="sans-serif"
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                >
+                    Stacks
+                </text>
+                {/* ------ line-1 ------ */}
+                <path
+                    id="line-1"
+                    d="
+                    M 350 250
+                    V 150
+                    L 300 100
+                    V 50
+                    M 295 45
+                    H 245"
+                    stroke="white"
+                    strokeWidth="2"
+                    fill="none"
+                />
+                <circle cx="300" cy="45" r="5" stroke="white" strokeWidth="2" fill="none" />
+                <circle cx="240" cy="45" r="5" stroke="white" strokeWidth="2" fill="none" />
 
-                {/* 백엔드 */}
-                <div className='
-                absolute
-                bottom-[5vh] left-[32vw]
-                rounded-full
-                h-[20vh] w-[20vh]
-                flex flex-col items-center justify-center
-                bg-black border-white border-4
-                transition-all ease-in-out duration-500
-                transform hover:scale-[2.8]
-                origin-bottom
-                group
-                '>
-                    {/* 기본텍스트 */}
-                    <div>
-                        <p className='
-                        font-semibold text-2xl
-                        group-hover:opacity-0
-                        transition-all duration-300
-                        '>Back-End</p>
-                    </div>
-                    {/* 백엔드 리스트 */}
-                    <div>
+                {Array.from({ length: 1000 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.8s" begin={`${i * 2}s`} repeatCount="1">
+                    <mpath href="#line-1" />
+                    </animateMotion>
+                </circle>
+                ))}
 
-                    </div>
-                </div>
-            </div>
-            <div className="max-w-4xl mx-auto px-4 text-center">
-                <h1 className="mb-4 text-5xl font-bold">Stacks</h1>
-            </div> 
+                {/* ------ line-2 ------ */}
+                <path 
+                    id="line-2"
+                    d="
+                    M 400 250
+                    V 200
+                    L 420 180
+                    V 30"
+                    stroke="white"
+                    stroke-width="2"
+                    fill="none"
+                />
+                <circle cx="420" cy="25" r="5" stroke="white" stroke-width="2" fill="none" />
+
+                {Array.from({ length: 1000 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.7s" begin={`${i * 2 + 0.5}s`} repeatCount="1">
+                    <mpath href="#line-2" />
+                    </animateMotion>
+                </circle>
+                ))}
+
+                {/* ------ line-3 ------ */}
+                <path 
+                    id="line-3"
+                    d="
+                    M 450 250
+                    V 225
+                    L 470 205
+                    H 550
+                    L 560 195"
+                    stroke="white"
+                    stroke-width="2"
+                    fill="none"
+                />
+
+                {Array.from({ length: 1000 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.5s" begin={`${i * 2 + 0.2}s`} repeatCount="1">
+                    <mpath href="#line-3" />
+                    </animateMotion>
+                </circle>
+                ))}
+
+                {/* ------ line-4 ------ */}
+                <path
+                    id="line-4"
+                    d="
+                    M 500 300
+                    H 650"
+                    stroke="white"
+                    stroke-width="2"
+                    fill="none"
+                />
+                <circle cx="655" cy="300" r="5" stroke="white" stroke-width="2" fill="none" />
+
+                {Array.from({ length: 1000 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.2s" begin={`${ i }`} repeatCount="1">
+                    <mpath href="#line-4" />
+                    </animateMotion>
+                </circle>
+                ))}                
+
+                {/* ------ line-5 ------ */}
+                <path
+                    id="line-5"
+                    d="
+                    M 450 350
+                    V 450
+                    L 500 500
+                    V 550"
+                    stroke="white"
+                    stroke-width="2"
+                    fill="none"
+                />
+                <circle cx="500" cy="555" r="5" stroke="white" stroke-width="2" fill="none" />
+
+                {Array.from({ length: 1000 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.7s" begin={`${i * 2 + 0.2}s`} repeatCount="1">
+                    <mpath href="#line-5" />
+                    </animateMotion>
+                </circle>
+                ))}   
+
+            </svg>
+        </div>
+        
+
         </section>
     )
 }
