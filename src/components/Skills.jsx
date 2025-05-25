461ea1c27e8b04f020419da30578d5e71a3fdfa5
kevinc02@naver.com <kevinc02@naver.com>
2025-05-25 14:56:59 +0900

    frontend circle

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -47,6 +47,7 @@ export default function Skills() {
                     x="400"
                     y="300"
                     fill="white"
+                    
                     font-size="40"
                     font-family="sans-serif"
                     text-anchor="middle"
@@ -71,7 +72,7 @@ export default function Skills() {
                 <circle cx="300" cy="45" r="5" stroke="white" strokeWidth="2" fill="none" />
                 <circle cx="240" cy="45" r="5" stroke="white" strokeWidth="2" fill="none" />
 
-                {Array.from({ length: 1000 }, (_, i) => (
+                {Array.from({ length: 500 }, (_, i) => (
                 <circle key={i} r="5" fill="black">
                     <animateMotion dur="0.8s" begin={`${i * 2}s`} repeatCount="1">
                     <mpath href="#line-1" />
@@ -93,9 +94,9 @@ export default function Skills() {
                 />
                 <circle cx="420" cy="25" r="5" stroke="white" stroke-width="2" fill="none" />
 
-                {Array.from({ length: 1000 }, (_, i) => (
+                {Array.from({ length: 500 }, (_, i) => (
                 <circle key={i} r="5" fill="black">
-                    <animateMotion dur="0.7s" begin={`${i * 2 + 0.5}s`} repeatCount="1">
+                    <animateMotion dur="0.7s" begin={`${i * 2 + 1.5}s`} repeatCount="1">
                     <mpath href="#line-2" />
                     </animateMotion>
                 </circle>
@@ -115,7 +116,7 @@ export default function Skills() {
                     fill="none"
                 />
 
-                {Array.from({ length: 1000 }, (_, i) => (
+                {Array.from({ length: 500 }, (_, i) => (
                 <circle key={i} r="5" fill="black">
                     <animateMotion dur="0.5s" begin={`${i * 2 + 0.2}s`} repeatCount="1">
                     <mpath href="#line-3" />
@@ -135,7 +136,7 @@ export default function Skills() {
                 />
                 <circle cx="655" cy="300" r="5" stroke="white" stroke-width="2" fill="none" />
 
-                {Array.from({ length: 1000 }, (_, i) => (
+                {Array.from({ length: 500 }, (_, i) => (
                 <circle key={i} r="5" fill="black">
                     <animateMotion dur="0.2s" begin={`${ i }`} repeatCount="1">
                     <mpath href="#line-4" />
@@ -157,13 +158,103 @@ export default function Skills() {
                 />
                 <circle cx="500" cy="555" r="5" stroke="white" stroke-width="2" fill="none" />
 
-                {Array.from({ length: 1000 }, (_, i) => (
+                {Array.from({ length: 500 }, (_, i) => (
                 <circle key={i} r="5" fill="black">
                     <animateMotion dur="0.7s" begin={`${i * 2 + 0.2}s`} repeatCount="1">
                     <mpath href="#line-5" />
                     </animateMotion>
                 </circle>
-                ))}   
+                ))}  
+
+                {/* ------ line-6 ------ */}
+                <path
+                    id="line-6"
+                    d="
+                    M 400 350
+                    V 430
+                    L 350 480
+                    V 580"
+                    stroke="white"
+                    stroke-width="2"
+                    fill="none"
+                />
+                <circle cx="350" cy="585" r="5" stroke="white" stroke-width="2" fill="none" />
+
+                {Array.from({ length: 500 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.4s" begin={`${i * 2 + 0.4}s`} repeatCount="1">
+                    <mpath href="#line-6" />
+                    </animateMotion>
+                </circle>
+                ))}
+
+                {/* ------ line-7 ------ */}
+                <path
+                    id="line-7"
+                    d="
+                    M 350 350
+                    V 400
+                    L 300 450
+                    V 550"
+                    stroke="white"
+                    stroke-width="2"
+                    fill="none"
+                />
+                <circle cx="300" cy="555" r="5" stroke="white" stroke-width="2" fill="none" />
+
+                {Array.from({ length: 500 }, (_, i) => (
+                <circle key={i} r="5" fill="black">
+                    <animateMotion dur="0.8s" begin={`${i * 2 + 1}s`} repeatCount="1">
+                    <mpath href="#line-7" />
+                    </animateMotion>
+                </circle>
+                ))}
+
+                <g className='group'>
+                    <circle cx="610" cy="145" r="70" stroke="white" stroke-width="2" fill="transparent" />
+
+                    <text                    
+                        x="610"
+                        y="150"
+                        fill="white"
+                        font-size="20"
+                        text-anchor="middle"
+                        dominant-baseline="middle"
+                    >
+                        Front-End
+                    </text>
+                    {[
+                        { icon: ReactIcon, angle: -120 },
+                        { icon: JSIcon, angle: -90 },
+                        { icon: HTMLIcon, angle: -60 },
+                        { icon: CSSIcon, angle: -30 },
+                        ].map(({ icon, angle }, idx) => {
+                        const radius = 100; // 펼쳐질 거리
+                        const rad = (angle * Math.PI) / 180;
+                        const x = 620 + radius * Math.cos(rad) - 15;
+                        const y = 150 + radius * Math.sin(rad) - 15;
+
+                        return (
+                            <image
+                            key={idx}
+                            href={icon}
+                            width="30"
+                            height="30"
+                            x={620 - 12} // 시작 위치는 중앙
+                            y={150 - 12}
+                            className="
+                                opacity-0
+                                group-hover:opacity-100
+                                transition-all duration-500
+                            "
+                            style={{
+                                transform: `translate(${x - 620}px, ${y - 135}px)`,
+                                transitionDelay: `${idx * 100}ms`,
+                            }}
+                            />
+                        );
+                    })}
+                </g>
 
             </svg>
         </div>
